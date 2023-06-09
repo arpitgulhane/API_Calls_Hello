969e45f5a1beab4591e05e68136a3c35e0931e4d
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-06-09 09:59:42 +0530

    name as parameter

--- src/main/java/com/hellomessaging/hellomessaging/controller/Hellomain.java
+++ src/main/java/com/hellomessaging/hellomessaging/controller/Hellomain.java
@@ -10,4 +10,10 @@ public class Hellomain {
         return "Hello from BridgeLabz";
     }
 
+//    @RequestMapping(value = {"/parameter"},method = RequestMethod.GET)
+    @GetMapping("/parameter")
+    public String parameterRequest(@RequestParam String name){
+        return "Hello  "+ name +" from BridgeLabz" ;
+    }
+
 }
