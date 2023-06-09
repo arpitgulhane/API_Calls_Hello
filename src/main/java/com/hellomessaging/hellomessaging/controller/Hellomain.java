7c40d2ffa10eae441afc54e8e228e809df3e3b3f
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-06-09 09:52:20 +0530

    uc1 show hello

--- src/main/java/com/hellomessaging/hellomessaging/controller/Hellomain.java
+++ src/main/java/com/hellomessaging/hellomessaging/controller/Hellomain.java
@@ -4,5 +4,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Hellomain {
+    //    @RequestMapping(value = {"","/","/hello"})
+    @GetMapping("/hello")
+    public String printhello(){
+        return "Hello from BridgeLabz";
+    }
 
 }
